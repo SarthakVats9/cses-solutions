8044c411ae7fefc01ce95a297e332b432cee3d0e
Sarthak Vats <sarthakvatssk@gmail.com>
2025-12-24 23:04:16 +0530

    24/12

---
+++ removal game 1.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+ 
+// ==================== MACROS ====================
+#define ll long long
+#define f(i, n) for (ll i = 0; i < (n); i++)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ia(a, n) ll a[n]; f(i, n) cin >> a[i]
+#define iv(v, n) vector<ll> v(n); f(i, n) cin >> v[i]
+ 
+const ll MOD = 1000000007;
+const ll INF = 1e18;
+ 
+// ==================== DEBUG ====================
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << nline;
+#else
+#define debug(x)
+#endif
+ 
+// ==================== SIEVE: SPF ====================
+ 
+int N = 1e6;
+vector<int> spf(N+1); // smallest prime factor for every number
+ 
+void buildSPF() {
+    for (int i = 2; i <= N; i++) spf[i] = i; // initially assume each number's SPF = itself
+ 
+    for (int i = 2; i * i <= N; i++) {
+        if (spf[i] == i) { // i is prime
+            for (int j = i * i; j <= N; j += i)
+                if (spf[j] == j) // not marked yet
+                    spf[j] = i;  // i is the smallest prime dividing j
+        }
+    }
+}
+int LIS(vector<ll> &a) {
+    
+int n = a.size();
+vector<int> dp; // dp[i] = smallest ending value of an IS of length i+1
+ 
+for (int x : a) {
+        auto it = lower_bound(dp.begin(), dp.end(), x);
+        if (it == dp.end()) dp.push_back(x);
+        else *it = x;
+    }
+    return dp.size();
+}
+ 
+bool cmp(const pair<int,int> &a, const pair<int,int> &b) {
+    return a.second < b.second;  }
+ //sort(range.begin(), range.end(), cmp);
+ 
+//logic is ez
+//implementation is ez
+//read question very carefully
+//if one approach doesnt work,,,try to think of all cases
+ 
+// ==================== SOLVE FUNCTION ====================
+ 
+ 
+void solve() {
+   int n ;
+   cin >> n ;
+   iv(v,n);
+   vector<vector<ll>>dp(n,vector<ll>(n));
+   for (int i = 0; i < n; i++) {
+    dp[i][i] = v[i];
+}
+
+   
+   if(n==1){
+       cout<<v[0]<<endl;
+       return;
+   }
+   
+   else if(n==2){
+       cout<<max(v[0],v[1])<<endl;
+       return;
+   }
+   if(n>=3){
+       dp[n-2][n-1]=max(v[n-1],v[n-2]);
+   for(int l=n-3; l>=0;l--){
+       for(int r =l+1;r<n;r++){
+           if(r-l==1){
+               dp[l][r]=max(v[l],v[r]);
+               continue;
+           }
+           ll a=0;
+           ll b =0;
+           if(l<=n-3&&r>=1){
+            a=v[l]+min(dp[l+1][r-1],dp[l+2][r]);
+           }
+           if(r>=2&&l<=n-2 ){
+            b=v[r]+min(dp[l+1][r-1],dp[l][r-2]);}
+           dp[l][r]=max(a,b);
+       }
+   }}
+   cout<<dp[0][n-1]<<endl;
+}
+// ==================== MAIN ====================
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    //buildSPF(); // Precompute
+ 
+    int t = 1;
+    
+    while (t--) solve();
+}
